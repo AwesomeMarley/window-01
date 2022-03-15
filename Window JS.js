ab13ea85d090a869b17d42843bcc83b99d7b4b8f
Brandon <86384607+AwesomeMarley@users.noreply.github.com>
2022-03-14 20:39:06 -0700

    Add files via upload

--- Window JS.js
+++ Window JS.js
@@ -1,71 +1,89 @@
-javascript: (function() {
-  
-  
-    /*frame = border,	innerframe = background,	title = title and blue bar at top,	innerpage = iframe which website displayed*/
-    
-    
+javascript: (function(){
+
     var style = document.createElement('style');
-    style.innerHTML = `
-    
+      style.innerHTML = `
+      
     body {
     overflow: hidden;
     }
     
-    
     #frame {
-    position: fixed;
-    width: 45%;
-    height: 45%;
-    
-    
-    margin: 0;
-    border-radius: 10px;
-    z-index: 1011;
-    border: 2px solid #0f99bb;
-    background: rgb(255, 0.5);
-    }
-    
-    #innerpage {
-    position: relative;
-  	bottom: -33px;
-		left: 3px;
-    
-    width: 43%;
-    height: 43%;
+    position: absolute;
+    width: 50%;
+    height: 50%;
+    top: 25%;
+    left: 25%;
 
     
-    
-    
-    border-radius: 10px;
-    z-index: 1009;
-    
-    
+    margin: 0;
+    padding: 0;
+    z-index: 99;
+    border: 5px soldid #0f99bb;
+    opacity = 0;
+    animation-name: startAnimation;
+    animation-duration: 2s;
+    animation-iteration-count: 1;
     }
     
     #title {
-    font-family: monospace;
-    background: #0077a7;
-    color: white;
-    font-size: 24px;
-    height: 30px;
+    vertical-align: text-top;
     text-align: center;
-    border-top-right-radius: 7px;
-    border-top-left-radius: 7px;
-    z-index: 1010;
+    font-family: monospace;
+    font-color: #8ed4f0;
+    background: #093344;
+    color: white;
+    font-size: 26px;
+    color: #1daae2;
+    width: calc(100% - 10px);
+    height: 30px;
     
+    padding: 5px;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+    overflow: hidden;
+    animation-name: startAnimation;
+  }
+
+  @keyframes startAnimation {
+    from {
+    opacity: 0;
+    top: 50%;
+    left: 50%;
+    width: 0px;
+    height: 0px
+    }
+    to {
+    opacity: 1;
+    }
+  }
+    
+    #innerpage{
+      position: absolute;
+      width: calc(100% - 10px);
+      height: calc(100% - 50px);
+      border-bottom-right-radius: 20px;
+      border-bottom-left-radius: 20px;
+      
+       
+      border-style: inset;
+      border-width: 5px;
+      animation-name: startAnimation;
+
     }
     
-    #innerframe {
+    #ghostframe {
     background: #999;
+    opacity: 0;
     
     width: 45%;
     height: 45%;
-
+    top: 20%;
+    left: 20%;
     
-    position: fixed;
+    position: absolute;
     margin: 0;
     padding: 0;
-    z-index: 1009;
+    z-index: 98;
     
     -webkit-transition: all 0.25s ease-in-out;
     -moz-transition: all 0.25s ease-in-out;
@@ -73,8 +91,8 @@ javascript: (function() {
     -o-transition: all 0.25s ease-in-out;
     transition: all 0.25s ease-in-out;
     }
-    `;
-    document.head.appendChild(style);
+      `;
+      document.head.appendChild(style);
     
     
     const frame_element = document.createElement('div');
@@ -83,27 +101,24 @@ javascript: (function() {
     
     const title_element = document.createElement('div');
     title_element.id = 'title';
-    title_element.textContent = 'Mini Page';
+    title_element.textContent = 'хаки';
     frame_element.append(title_element);
     
-    const innerframe_element = document.createElement('div');
-    innerframe_element.id = 'innerframe';
-    document.body.append(innerframe_element);
-      
+    const ghostframe_element = document.createElement('div');
+    ghostframe_element.id = 'ghostframe';
+    document.body.append(ghostframe_element);
+    
     const innerpage_element = document.createElement('iframe');
-    innerpage_element.id = 'innerpage';
-    innerpage_element.src="https://classic.minecraft.net";
-    document.body.append(innerpage_element);
+    innerpage_element.id = 'innerpage'; innerpage_element.src="https://www.bing.com/";
+     frame_element.append(innerpage_element);
     
     
-    
-    
-    var minWidth = 80;
-    var minHeight = 60;
+    var minWidth = 400;
+    var minHeight = 250;
     
     
     var FULLSCREEN_MARGINS = -10;
-    var MARGINS = 4;
+    var MARGINS = 6;
     
     
     var clicked = null;
@@ -118,22 +133,23 @@ javascript: (function() {
     var redraw = false;
     
     var frame = document.getElementById('frame');
-    var innerframe = document.getElementById('innerframe');
+    var ghostframe = document.getElementById('ghostframe');
     var page = document.getElementById('innerpage');
     
     function setBounds(element, x, y, w, h) {
-      element.style.left = x + 'px';
-      element.style.top = y + 'px';
-      element.style.width = w + 'px';
-      element.style.height = h + 'px';
+        element.style.left = x + 'px';
+        element.style.top = y + 'px';
+        element.style.width = w + 'px';
+        element.style.height = h + 'px';
     }
     
     function hintHide() {
-    setBounds(innerframe, b.left, b.top, b.width, b.height);
-    innerframe.style.opacity = 0;
-
+      setBounds(ghostframe, b.left, b.top, b.width, b.height);
+      ghostframe.style.opacity = 0;
+    
     
     }
+    
     
     
     frame.addEventListener('mousedown', onMouseDown);
@@ -147,234 +163,210 @@ javascript: (function() {
     
     
     function onTouchDown(e) {
-    onDown(e.touches[0]);
-    e.preventDefault();
+      onDown(e.touches[0]);
+      e.preventDefault();
     }
     
     function onTouchMove(e) {
-    onMove(e.touches[0]);		
+      onMove(e.touches[0]);		
     }
     
     function onTouchEnd(e) {
-    if (e.touches.length ==0) onUp(e.changedTouches[0]);
+      if (e.touches.length ==0) onUp(e.changedTouches[0]);
     }
     
     function onMouseDown(e) {
-    onDown(e);
-    e.preventDefault();
+      onDown(e);
+      e.preventDefault();
     }
     
     function onDown(e) {
-    calc(e);
+      calc(e);
     
-    var isResizing = onRightEdge || onBottomEdge || onTopEdge || onLeftEdge;
+      var isResizing = onRightEdge || onBottomEdge || onTopEdge || onLeftEdge;
     
-    clicked = {
-      x: x,
-      y: y,
-      cx: e.clientX,
-      cy: e.clientY,
-      w: b.width,
-      h: b.height,
-      isResizing: isResizing,
-      isMoving: !isResizing && canMove(),
-      onTopEdge: onTopEdge,
-      onLeftEdge: onLeftEdge,
-      onRightEdge: onRightEdge,
-      onBottomEdge: onBottomEdge
-    };
+      clicked = {
+        x: x,
+        y: y,
+        cx: e.clientX,
+        cy: e.clientY,
+        w: b.width,
+        h: b.height,
+        isResizing: isResizing,
+        isMoving: !isResizing && canMove(),
+        onTopEdge: onTopEdge,
+        onLeftEdge: onLeftEdge,
+        onRightEdge: onRightEdge,
+        onBottomEdge: onBottomEdge
+      };
     }
     
     function canMove() {
-    return x > 0 && x < b.width && y > 0 && y < b.height
-    && y < 30;
+      return x > 0 && x < b.width && y > 0 && y < b.height
+      && y < 30;
     }
     
     function calc(e) {
-    b = frame.getBoundingClientRect();
-    x = e.clientX - b.left;
-    y = e.clientY - b.top;
+      b = frame.getBoundingClientRect();
+      x = e.clientX - b.left + MARGINS/2;
+      y = e.clientY - b.top + MARGINS/2;
     
-    onTopEdge = y < MARGINS;
-    onLeftEdge = x < MARGINS;
-    onRightEdge = x >= b.width - MARGINS;
-    onBottomEdge = y >= b.height - MARGINS;
+      onTopEdge = y < MARGINS;
+      onLeftEdge = x < MARGINS;
+      onRightEdge = x >= b.width - MARGINS;
+      onBottomEdge = y >= b.height - MARGINS;
     
-    rightScreenEdge = window.innerWidth - MARGINS;
-    bottomScreenEdge = window.innerHeight - MARGINS;
+      rightScreenEdge = window.innerWidth - MARGINS;
+      bottomScreenEdge = window.innerHeight - MARGINS;
     }
     
     var e;
     
     function onMove(ee) {
-    calc(ee);
+      calc(ee);
     
-    e = ee;
+      e = ee;
     
-    redraw = true;
+      redraw = true;
     
     }
     
     function animate() {
     
-    requestAnimationFrame(animate);
+      requestAnimationFrame(animate);
     
-    if (!redraw) return;
+      if (!redraw) return;
     
-    redraw = false;
+      redraw = false;
     
-    if (clicked && clicked.isResizing) {
+      if (clicked && clicked.isResizing) {
     
-      if (clicked.onRightEdge) frame.style.width = Math.max(x, minWidth) + 'px';
-      if (clicked.onBottomEdge) frame.style.height = Math.max(y, minHeight) + 'px';
-      if (clicked.onRightEdge) page.style.width = Math.max(x, minWidth) + 'px';
-      if (clicked.onBottomEdge) page.style.height = Math.max(y, minHeight) + 'px';
+        if (clicked.onRightEdge){ frame.style.width = Math.max(x, minWidth) + 'px'; }
+        if (clicked.onBottomEdge){ frame.style.height = Math.max(y, minHeight) + 'px'; }
     
-      if (clicked.onLeftEdge) {
-        var currentWidth = Math.max(clicked.cx - e.clientX  + clicked.w, minWidth);
-        if (currentWidth > minWidth) {
-          frame.style.width = currentWidth + 'px';
-          frame.style.left = e.clientX + 'px'; 
-          page.style.width = currentWidth + 'px';
-          page.style.left = e.clientX + 'px';	
+        if (clicked.onLeftEdge) {
+          var currentWidth = Math.max(clicked.cx - e.clientX  + clicked.w, minWidth);
+          if (currentWidth > minWidth) {
+            frame.style.width = currentWidth + 'px';
+            frame.style.left = e.clientX + 'px';
+          }
         }
-      }
     
-      if (clicked.onTopEdge) {
-        var currentHeight = Math.max(clicked.cy - e.clientY  + clicked.h, minHeight);
-        if (currentHeight > minHeight) {
-          frame.style.height = currentHeight + 'px';
-          frame.style.top = e.clientY + 'px';	
-          page.style.height = currentHeight + 'px';
-          page.style.top = e.clientY + 'px';	
+        if (clicked.onTopEdge) {
+          var currentHeight = Math.max(clicked.cy - e.clientY  + clicked.h, minHeight);
+          if (currentHeight > minHeight) {
+            frame.style.height = currentHeight + 'px';
+            frame.style.top = e.clientY + 'px';	
+          }
         }
-      }
     
-      hintHide();
-    
-      return;
-    }
-    
-    if (clicked && clicked.isMoving) {
-    
-      if (b.top < FULLSCREEN_MARGINS || b.left < FULLSCREEN_MARGINS || b.right > window.innerWidth - FULLSCREEN_MARGINS || b.bottom > window.innerHeight - FULLSCREEN_MARGINS) {
-    
-        setBounds(innerframe, 0, 0, window.innerWidth, window.innerHeight);
-        innerframe.style.opacity = 0.2;
-      } else if (b.top < MARGINS) {
-    
-        setBounds(innerframe, 0, 0, window.innerWidth, window.innerHeight / 2);
-        innerframe.style.opacity = 0.2;
-      } else if (b.left < MARGINS) {
-    
-        setBounds(innerframe, 0, 0, window.innerWidth / 2, window.innerHeight);
-        innerframe.style.opacity = 0.2;
-      } else if (b.right > rightScreenEdge) {
-    
-        setBounds(innerframe, window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
-        innerframe.style.opacity = 0.2;
-      } else if (b.bottom > bottomScreenEdge) {
-    
-        setBounds(innerframe, 0, window.innerHeight / 2, window.innerWidth, window.innerWidth / 2);
-        innerframe.style.opacity = 0.2;
-      } else {
         hintHide();
+    
+        return;
       }
     
-      if (preSnapped) {
-        setBounds(frame,
-            e.clientX - preSnapped.width / 2,
-            e.clientY - Math.min(clicked.y, preSnapped.height),
-            preSnapped.width,
-            preSnapped.height
-        );
-        setBounds(page,
-            e.clientX - preSnapped.width / 2,
-            e.clientY - Math.min(clicked.y, preSnapped.height),
-            preSnapped.width,
-            preSnapped.height
-        );
+      if (clicked && clicked.isMoving) {
+    
+        if (b.top < FULLSCREEN_MARGINS || b.left < FULLSCREEN_MARGINS || b.right > window.innerWidth - FULLSCREEN_MARGINS || b.bottom > window.innerHeight - FULLSCREEN_MARGINS) {
+          
+          setBounds(ghostframe, 0, 0, window.innerWidth, window.innerHeight);
+          ghostframe.style.opacity = 0.2;
+        } else if (b.top < MARGINS) {
+          
+          setBounds(ghostframe, 0, 0, window.innerWidth, window.innerHeight / 2);
+          ghostframe.style.opacity = 0.2;
+        } else if (b.left < MARGINS) {
+          
+          setBounds(ghostframe, 0, 0, window.innerWidth / 2, window.innerHeight);
+          ghostframe.style.opacity = 0.2;
+        } else if (b.right > rightScreenEdge) {
+          
+          setBounds(ghostframe, window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
+          ghostframe.style.opacity = 0.2;
+        } else if (b.bottom > bottomScreenEdge) {
+          
+          setBounds(ghostframe, 0, window.innerHeight / 2, window.innerWidth, window.innerWidth / 2);
+          ghostframe.style.opacity = 0.2;
+        } else {
+          hintHide();
+        }
+    
+        if (preSnapped) {
+          setBounds(frame,
+              e.clientX - preSnapped.width / 2,
+              e.clientY - Math.min(clicked.y, preSnapped.height),
+              preSnapped.width,
+              preSnapped.height
+          );
+          return;
+        }
+    
+    
+        frame.style.top = (e.clientY - clicked.y) + 'px';
+        frame.style.left = (e.clientX - clicked.x) + 'px';
+    
         return;
       }
     
     
-      frame.style.top = (e.clientY - clicked.y) + 'px';
-      frame.style.left = (e.clientX - clicked.x) + 'px';
-      page.style.top = (e.clientY - clicked.y) + 'px';
-      page.style.left = (e.clientX - clicked.x) + 'px';
-    
-      return;
-    }
-    
-    
-    if (onRightEdge && onBottomEdge || onLeftEdge && onTopEdge) {
-      frame.style.cursor = 'nwse-resize';
-      page.style.cursor = 'nwse-resize';
-    } else if (onRightEdge && onTopEdge || onBottomEdge && onLeftEdge) {
-      frame.style.cursor = 'nesw-resize';
-      page.style.cursor = 'nesw-resize';
-    } else if (onRightEdge || onLeftEdge) {
-      frame.style.cursor = 'ew-resize';
-      page.style.cursor = 'ew-resize';
-    } else if (onBottomEdge || onTopEdge) {
-      frame.style.cursor = 'ns-resize';
-      page.style.cursor = 'ns-resize';
-    } else if (canMove()) {
-      frame.style.cursor = 'move';
-      page.style.cursor = 'move';
-    } else {
-      frame.style.cursor = 'default';
-      page.style.cursor = 'default';
-    }
+      if (onRightEdge && onBottomEdge || onLeftEdge && onTopEdge) {
+        frame.style.cursor = 'nwse-resize';
+      } else if (onRightEdge && onTopEdge || onBottomEdge && onLeftEdge) {
+        frame.style.cursor = 'nesw-resize';
+      } else if (onRightEdge || onLeftEdge) {
+        frame.style.cursor = 'ew-resize';
+      } else if (onBottomEdge || onTopEdge) {
+        frame.style.cursor = 'ns-resize';
+      } else if (canMove()) {
+        frame.style.cursor = 'move';
+      } else {
+        frame.style.cursor = 'default';
+      }
     }
     
     animate();
     
     function onUp(e) {
-    calc(e);
+      calc(e);
     
-    if (clicked && clicked.isMoving) {
+      if (clicked && clicked.isMoving) {
+        
+        var snapped = {
+          width: b.width,
+          height: b.height
+        };
     
-      var snapped = {
-        width: b.width,
-        height: b.height
-      };
+        if (b.top < FULLSCREEN_MARGINS || b.left < FULLSCREEN_MARGINS || b.right > window.innerWidth - FULLSCREEN_MARGINS || b.bottom > window.innerHeight - FULLSCREEN_MARGINS) {
+          
+          setBounds(frame, 0, 0, window.innerWidth, window.innerHeight);
+          preSnapped = snapped;
+        } else if (b.top < MARGINS) {
+          
+          setBounds(frame, 0, 0, window.innerWidth, window.innerHeight / 2);
+          preSnapped = snapped;
+        } else if (b.left < MARGINS) {
+          
+          setBounds(frame, 0, 0, window.innerWidth / 2, window.innerHeight);
+          preSnapped = snapped;
+        } else if (b.right > rightScreenEdge) {
+          
+          setBounds(frame, window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
+          preSnapped = snapped;
+        } else if (b.bottom > bottomScreenEdge) {
+          
+          setBounds(frame, 0, window.innerHeight / 2, window.innerWidth, window.innerWidth / 2);
+          preSnapped = snapped;
+        } else {
+          preSnapped = null;
+        }
     
-      if (b.top < FULLSCREEN_MARGINS || b.left < FULLSCREEN_MARGINS || b.right > window.innerWidth - FULLSCREEN_MARGINS || b.bottom > window.innerHeight - FULLSCREEN_MARGINS) {
+        hintHide();
     
-        setBounds(frame, 0, 0, window.innerWidth, window.innerHeight);
-        setBounds(page, 0, 0, window.innerWidth, window.innerHeight);
-        preSnapped = snapped;
-      } else if (b.top < MARGINS) {
-    
-        setBounds(frame, 0, 0, window.innerWidth, window.innerHeight / 2);
-        setBounds(page, 0, 0, window.innerWidth, window.innerHeight / 2);
-        preSnapped = snapped;
-      } else if (b.left < MARGINS) {
-    
-        setBounds(frame, 0, 0, window.innerWidth / 2, window.innerHeight);
-        setBounds(page, 0, 0, window.innerWidth / 2, window.innerHeight);
-        preSnapped = snapped;
-      } else if (b.right > rightScreenEdge) {
-    
-        setBounds(frame, window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
-        setBounds(page, window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
-        preSnapped = snapped;
-      } else if (b.bottom > bottomScreenEdge) {
-    
-        setBounds(frame, 0, window.innerHeight / 2, window.innerWidth, window.innerWidth / 2);
-        setBounds(page, 0, window.innerHeight / 2, window.innerWidth, window.innerWidth / 2);
-        preSnapped = snapped;
-      } else {
-        preSnapped = null;
       }
     
-      hintHide();
+      clicked = null;
     
     }
+      
+    })();
     
-    clicked = null;
-    
-    }
-    
-    })()
